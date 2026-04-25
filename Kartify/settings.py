06fa36d4163bb1b738b21ba3cb2be56a061ee10b
Harshit <harshit.tiwari0413@gmail.com>
2026-04-25 22:57:49 +0530

    Login Fuctionality and account activation

--- Kartify/settings.py
+++ Kartify/settings.py
@@ -139,3 +139,11 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+
+# SMTP Cobnfiguration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'tiwariharshit1306@gmail.com'
+EMAIL_HOST_PASSWORD = 'Hars@#4321'
+EMAIL_USE_TLS = True
